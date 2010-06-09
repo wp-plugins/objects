e1ea8b5aba8607edd323062d02417b010f25cc43
frankieroberto <frankieroberto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-09 17:24:25 +0000

    Adding custom fields as an option.
    git-svn-id: https://plugins.svn.wordpress.org/objects/trunk@250343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- objects.php
+++ objects.php
@@ -40,17 +40,24 @@ function post_type_objects() {
 								    'capability_type' => 'post',										
 										'publicly_queryable' => true,
 										'exclude_from_search' => false,
-										'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions'),
+										'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'comments', 'revisions'),
 										'rewrite' => array('slug' => 'objects'),
 										'can_export' => true
 										));
 	 register_taxonomy( 'object-tags', 'object', array( 'hierarchical' => false, 'label' => __('Tags'), 'public' => true,	'show_ui' => true, 'show_tagclour' => true) ); 
 	 register_taxonomy( 'collections', 'object', array( 'hierarchical' => true, 'label' => __('Collections'), 'labels' => array('singular_name' => 'Collection', 'search_items' => 'Search Collections', 'popular_items' => 'Popular Collections', 'all_items' => 'All Collections', 'parent_item' => 'Parent Collection', 'parent_item_colon' => 'Parent Collection:', 'edit_item' => 'Edit Collection', 'update_item' => 'Update Collection', 'add_new_item' => 'Add Collection', 'new_item_name' => 'New Collection Name'), 'public' => true,	'show_ui' => true 	) ); 
+
+	 register_taxonomy( 'object-types', 'object', array( 'hierarchical' => true, 'labels' => array('name' => 'Object Types', 'singular_name' => 'Object Type', 'search_items' => 'Search Object Types', 'popular_items' => 'Popular Object Types', 'all_items' => 'All Object Types', 'parent_item' => 'Parent Object Type', 'parent_item_colon' => 'Parent Object Type:', 'edit_item' => 'Edit Object Type', 'update_item' => 'Update Object Type', 'add_new_item' => 'Add Object Type', 'new_item_name' => 'New Object Type'), 'public' => true,	'show_ui' => true 	) ); 
+
+	 register_taxonomy( 'object-materials', 'object', array( 'hierarchical' => true, 'labels' => array('name' => 'Materials', 'singular_name' => 'Material', 'add_new_item' => 'Add Material', 'parent_item' => 'Parent Material', 'all_items' => 'All Materials'), 'public' => true,	'show_ui' => true 	) ); 
+
+	if (get_option('object-show-custom-fields'))  {
+		add_post_type_support('object', 'custom-fields');
+	}
 										
 }
 
 
-add_action('init', 'post_type_objects');
 
 function object_info_meta_box() {
   global $post;
@@ -69,25 +76,90 @@ function object_info_meta_box() {
 
 }
 
+function objects_right_now_content() {
+		$num_objects = wp_count_posts( 'object' );
+		$num_collections = wp_count_terms('collections');
+		$num_object_types = wp_count_terms('object-types');
+		$num_object_tags = wp_count_terms('object-tags');
+		$num_object_materials = wp_count_terms('object-materials');
+		
+		echo "\n\t".'<tr>';
+
+		// Objects
+		$num = number_format_i18n( $num_objects->publish );
+		$text = _n( 'Object', 'Objects', intval($num_objects->publish) );
+		if ( current_user_can( 'edit_posts' ) ) {
+			$num = "<a href='edit.php?post_type=objects'>$num</a>";
+			$text = "<a href='edit.php?post_type=objects'>$text</a>";
+		}		
+		
+		echo '<td class="first b b-posts">' . $num . '</td>';
+		echo '<td class="t posts">' . $text . '</td>';
+		echo '</tr><tr>';
+
+		// Collections
+		$num = number_format_i18n( $num_collections );
+		$text = _n( 'Collection', 'Collections', $num_collections );
+		if ( current_user_can( 'manage_categories' ) ) {
+			$num = "<a href='edit-tags.php?taxonomy=collections'>$num</a>";
+			$text = "<a href='edit-tags.php?taxonomy=collections'>$text</a>";
+		}
+		echo '<td class="first b b-cats">' . $num . '</td>';
+		echo '<td class="t cats">' . $text . '</td>';
+
+		echo '</tr><tr>';
+		
+		// Object types
+		$num = number_format_i18n( $num_object_types );
+		$text = _n( 'Object Type', 'Object Types', $num_object_types );
+		if ( current_user_can( 'manage_categories' ) ) {
+			$num = "<a href='edit-tags.php?taxonomy=object-types'>$num</a>";
+			$text = "<a href='edit-tags.php?taxonomy=object-types'>$text</a>";
+		}
+		echo '<td class="first b b-cats">' . $num . '</td>';
+		echo '<td class="t cats">' . $text . '</td>';
+
+		echo '</tr><tr>';		
+
+		// Object tags
+		$num = number_format_i18n( $num_object_tags );
+		$text = _n( 'Object Tag', 'Object Tags', $num_object_tags );
+		if ( current_user_can( 'manage_categories' ) ) {
+			$num = "<a href='edit-tags.php?taxonomy=object-tags'>$num</a>";
+			$text = "<a href='edit-tags.php?taxonomy=object-tags'>$text</a>";
+		}
+		echo '<td class="first b b-cats">' . $num . '</td>';
+		echo '<td class="t cats">' . $text . '</td>';
+
+		echo '</tr><tr>';		
+
+		// Materials
+		$num = number_format_i18n( $num_object_materials );
+		$text = _n( 'Material', 'Materials', $num_object_materials );
+		if ( current_user_can( 'manage_categories' ) ) {
+			$num = "<a href='edit-tags.php?taxonomy=materials'>$num</a>";
+			$text = "<a href='edit-tags.php?taxonomy=materials'>$text</a>";
+		}
+		echo '<td class="first b b-cats">' . $num . '</td>';
+		echo '<td class="t cats">' . $text . '</td>';
+
+		echo '</tr>';
+		
+}
+
+
 
 function object_info_save( $post_id ) {
+
   // verify this came from the our screen and with proper authorization,
   // because save_post can be triggered at other times
-
   if ( !wp_verify_nonce( $_POST['created_info_noncename'], plugin_basename(__FILE__) )) {
     return $post_id;
   }
 
-
   // OK, we're authenticated: we need to find and save the data
-
   $created_date = $_POST['object-created-date'];
   $colour = $_POST['object-colour'];
-
-
-  // TODO: Do something with $mydata 
-  // probably using add_post_meta(), update_post_meta(), or 
-  // a custom table (see Further Reading section below)
 
   update_post_meta($post_id, 'object-created-date', $created_date);
   update_post_meta($post_id, 'object-colour', $colour);  
@@ -127,19 +199,67 @@ function show_object_settings_page() {
 	
 	?>	<div class="wrap"><?php screen_icon(); ?>
 	<h2><?php echo esc_html( 'Object Settings' ); ?></h2>	
+		
+		<form method="post" action="options.php">
+
+			<?php settings_fields('object-settings'); ?>
+			<?php do_settings_sections('object-settings'); ?>
+			
+			
+			<p class="submit">
+			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+			</p>
+			</form>
+			
+		</form>
 		</div>
 	<?php
 }
 
-function add_object_settings() {
-	add_options_page('Object settings', 'Objects', 'manage_options', 'object_settings', 'show_object_settings_page');
-}
 
-
+add_action('init', 'post_type_objects');
 add_action('admin_menu', 'add_object_meta_boxes');
 add_action('admin_menu', 'add_object_settings');
 add_action('save_post', 'object_info_save');
+add_action('right_now_content_table_end', 'objects_right_now_content');
 
 
-
+function add_object_settings() {
+ 	// Add the section to reading settings so we can add our fields to it
+ 	add_settings_section('eg_setting_section', 'Object Settings', 'object_settings_section', 'general');
+ 	
+ 	// Add the field with the names and function to use for our new settings, put it in our new section
+ 	add_settings_field('object-show-custom-fields', 'Show Custom Fields?', 'eg_setting_callback_function', 'general', 'eg_setting_section');
+ 	
+ 	// Register our setting so that $_POST handling is done for us and our callback function just has to echo the <input>
+ 	register_setting('general','object-show-custom-fields');
+ }// eg_settings_api_init()
+ 
+ 
+  
+ // ------------------------------------------------------------------
+ // Settings section callback function
+ // ------------------------------------------------------------------
+ //
+ // This function is needed if we added a new section. This function 
+ // will be run at the start of our section
+ //
+ 
+ function object_settings_section() {
+	// 	echo '<p>These settings relate to <a href="edit.php?post_type=object">Objects</a>.</p>';
+ }
+ 
+ 
+function eg_setting_callback_function() {
+ 	$checked = "";
+ 	
+ 	// Mark our checkbox as checked if the setting is already true
+ 	if (get_option('object-show-custom-fields')) 
+ 		$checked = " checked='checked' ";
+ 
+	echo '<label for="object-show-custom-fields">';
+ 	echo "<input {$checked} name='object-show-custom-fields' id='object-show-custom-fields' type='checkbox' /> ";
+	echo "This allows authors to any extra fields they like, but also reveals some default fields.";
+	echo "</label>";
+ } // eg_setting_callback_function()
 ?>
